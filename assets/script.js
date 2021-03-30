38ec7990549acab0d29b9720bac8bea037e6ab4c
Jon W <jonwaaler@outlook.com>
2021-03-29 23:47:30 -0400

    made a fetch to get weather an console.log it

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
-//
+var apiKey = "763af95ff91c9e580b55c19df288e7b3";
+var cityName = "toronto";
+var units = "metric";
+
+function GetURL() {
+  return `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&unitys=${units}`;
+}
+
+fetch(GetURL())
+  .then(function (response) {
+    //promise === callback
+    return response.json(); //This also returns a promise
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(data.list[0].main.temp);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
